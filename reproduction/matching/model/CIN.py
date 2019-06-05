bf9572e2c1163c8cebc769bd4721b7f8b54d28fa
JerrikEph <jerrikeph@gmail.com>
2019-06-05 23:07:12 +0800

    add CIN

---
+++ reproduction/matching/model/CIN.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from fastNLP.models import BaseModel
+
+#args = type('args', (), {})()
+
+class CIN4NLI(BaseModel):
+    # TODO: still in progress
+
+    def __init(self):
+        super(CIN4NLI, self).__init__()
+
+    def forward(self, words, segment_id, seq_len):
+        pass
+
+
+class CIN(nn.Module):
+    def __init(self):
+        super(CIN, self).__init__()
+
+    def forward(self, words, segment_id, seq_len):
+        pass
