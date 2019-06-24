25eca244061cc49ab7a0aafc518545e0180356b6
JerrikEph <jerrikeph@gmail.com>
2019-06-25 00:04:54 +0800

    add CIN Model, still debugging

--- reproduction/matching/model/CIN.py
+++ reproduction/matching/model/CIN.py
@@ -290,12 +290,12 @@ class InterativeConv(nn.Module):
             if i < 0:
                 pdNum = int(np.abs(i))
                 o = torch.cat([pad]*pdNum + [inputs], dim=1)
-                collect.append(o[:, :, :tstp])
+                collect.append(o[:, :tstp, :])
             elif i == 0:
                 collect.append(inputs)
             else:
                 pdNum = int(np.abs(i))
                 o = torch.cat([inputs] + [pad] * pdNum, dim=1)
-                collect.append(o[:, :, -tstp:])
-        out = torch.cat(collect, dim=-1)
+                collect.append(o[:, -tstp:, :])
+        out = torch.cat(collect, dim=1)
         return out
