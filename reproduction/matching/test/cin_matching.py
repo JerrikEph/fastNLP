48b4bb33baad2a09dd906acf9b50522f45a09f2d
JerrikEph <jerrikeph@gmail.com>
2019-07-02 16:45:43 +0800

    update to adamax

--- reproduction/matching/test/cin_matching.py
+++ reproduction/matching/test/cin_matching.py
@@ -115,7 +115,7 @@ else:
     raise RuntimeError(f'NOT support {arg.model} model yet!')
 
 
-optimizer = AdamW(lr=arg.lr, params=model.parameters(), weight_decay=arg.wd)
+optimizer = Adamax(lr=arg.lr, params=model.parameters())
 scheduler = StepLR(optimizer, step_size=10, gamma=0.5)
 
 callbacks = [
